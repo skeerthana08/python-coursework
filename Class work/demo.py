926cef23a764aa6073072a13796297f8e8f8bda8
skeerthana08 <keerthana781604@gmail.com>
2025-07-07 15:00:07 +0530

    first program

---
+++ Class work/demo.py
@@ -0,0 +1,28 @@
+#4.Python operators
+#1.Arithmetic operators
+
+a=20
+b=30
+
+print("addition of a and  b:" , a+b)
+print("subtraction of a and  b:", a-b)
+print("muultiplication of a and  b:",  a*b)
+print("division of a and  b:", a/b)
+print("float division of a and  b:", a//b)
+print("modulus of a and  b:",a%b)
+print("exponentation of a and  b:", a**b)
+
+#2.Comparision operators
+
+c=56
+d=23
+
+c==d
+
+print("Equal to(c==d):", c==d)  
+print("Not Equal to(c!=d):", c!=d)
+print("Greater than(c>d):", c>d)
+print("Less than(c<d):", c<d)
+print("Greater than or equal to(c>=d):", c>=d)
+print("Less than or equal to(c<=d):", c<=d)
+

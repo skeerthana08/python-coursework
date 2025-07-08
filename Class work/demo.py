355ffe7aa18ba4a0fcd7b8cab44c797833f9b0ed
skeerthana08 <keerthana781604@gmail.com>
2025-07-08 16:01:07 +0530

    folders created

--- Class work/demo.py
+++ Class work/demo.py
@@ -1,22 +1,27 @@
-#3.Assignment operators
-'''var = var (op) val
-var (op) = val'''
+name="keerthana"
+name
+'w' in name
+'e' in name
+'s' in name
+'kee' in name
+'rteek' in name
 
+l = [1,2,3]
+l
+3 not in l
+ 
+t = (4,5,6)
+t
+5 in t 
 
-x=5 #assign value to variable
-x+=10
-x
-x-=10
-x
-x*=10
-x
-x/=10
-x
-x//=10
-x
-x%=10
-x
-x**=2
-x
+s={'pfs', 'jfs', 'ds'}
+s
+'ds' in s
+'da' in s
+data = {'name':'keer', 'age': 21}
+data
+'name' in data
+'keer' in data #only keys can be checked 
+'course' not in data
 
 

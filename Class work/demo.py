0556fdb58671c0b013d95169fc1f78b8cc338ba7
skeerthana08 <keerthana781604@gmail.com>
2025-07-08 16:18:55 +0530

    first program

--- Class work/demo.py
+++ Class work/demo.py
@@ -1,3 +1,4 @@
+#5. Membership Operators
 name="keerthana"
 name
 'w' in name
@@ -25,3 +26,47 @@ data
 'course' not in data
 
 
+#6.identity operators
+
+n=[1,2,3]
+m=[1,2,3]
+id(n)
+id(m)
+n = l
+n is m 
+n is l
+m is n
+l is n
+id(l)
+n is not m 
+
+
+#4.Python operators
+#1.Arithmetic operators
+
+a=20
+a
+b=30
+b
+print("addition of a and  b:", a+b)  
+print("subtraction of a and  b:", a-b)
+print("muultiplication of a and  b:",  a*b)
+print("division of a and  b:", a/b)
+print("float division of a and  b:", a//b)
+print("modulus of a and  b:",a%b)
+print("exponentation of a and  b:", a**b)
+
+
+
+#2.Comparision operators
+
+c=56
+c
+d=23
+d
+print("Equal to(c==d):", c==d)
+print("Not Equal to(c!=d):", c!=d)
+print("Greater than(c>d):", c>d)
+print("Less than(c<d):", c<d)
+print("Greater than or equal to(c>=d):", c>=d)
+print("Less than or equal to(c<=d):", c<=d)
